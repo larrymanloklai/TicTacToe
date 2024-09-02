c100d9aa74c120874729f3a5f34ba8e3e7fc585d
larrymanloklai <larrymanlok.lai@gmail.com>
2024-09-02 22:46:26 +1000

    finish drafting the main structure of html js and css; but there are errors need to be resolved.

--- script.js
+++ script.js
@@ -1,31 +1,110 @@
-//there are 4 objects in this script: 1 gamePlayer, 2 gameController, 3 gameBoard, 4 displayControl
-
 // 1 gamePlayer object constructor
 // Players: Each player should have a name and a mark (either 'X' or 'O').
 function gamePlayer(name, mark) {
-    this.name = name
+    this.name = name;
     this.mark = mark;
 }
 
-// 2 gameController
-const gameController = (function() {
+// 2 gameBoard: It needs to be represented as an array and should be able to reset.
+// Manage the state of the gameBoard.
+const gameBoard = (function () {
+    let board = Array(9).fill('');
+
+    const setField = (index, mark) => {
+        if (index >= 0 && index < board.length && board[index] === '') {
+            board[index] = mark;
+            return true;
+        }
+        return false;
+    };
+
+    const getField = (index) => {
+        return board[index];
+    };
+
+    const resetBoard = () => {
+        board = Array(9).fill('');
+    };
+
+    const getBoard = () => {
+        return board;
+    };
+
+    return { setField, getField, resetBoard, getBoard };
+})();
+
+// 3 displayController Manage game flow, check for a winner, handle player turns, and update the game status.
+
+const displayController = (function () {
+    const gameBoardElement = document.getElementById('gameBoard');
+    const gameStatusElement = document.getElementById('gameStatus');
+    const startButton = document.getElementById('startButton');
+    const restartButton = document.getElementById('restartButton');
+
+    const drawBoard = () => {
+        gameBoardElement.innerHTML = gameBoard.getBoard().map((mark, index) => `
+            <div class="book" data-index="${index}">${mark}</div>
+        `).join('');
+    };
+
+    const updateGameStatus = (message) => {
+        gameStatusElement.textContent = message;
+    };
+
+    startButton.addEventListener('click', () => {
+        const gamePlayerOneName = document.getElementById('player1').value || 'Player 1';
+        const gamePlayerTwoName = document.getElementById('player2').value || 'Player 2';
+        gameController.startGame(gamePlayerOneName, gamePlayerTwoName);
+    });
+
+    restartButton.addEventListener('click', () => {
+        const gamePlayerOneName = document.getElementById('player1').value || 'Player 1';
+        const gamePlayerTwoName = document.getElementById('player2').value || 'Player 2';
+        gameController.startGame(gamePlayerOneName, gamePlayerTwoName);
+    });
+
+    gameBoardElement.addEventListener('click', (e) => {
+        if (e.target.matches('.book')) {
+            const index = parseInt(e.target.dataset.index, 10);
+            gameController.inGame(index);
+        }
+    });
+
+    return { drawBoard, updateGameStatus };
+})();
+
+// 4 gameController
+const gameController = (function () {
     let gamePlayers = [];
     let currentPlayerIndex = 0;
     let gameOver = false;
 
-    const startGame (gamePlayerOneName, gamePlayerTwoName) {
-        gamePlayers = [gamePlayer(gamePlayerOneName, 'X'), gamePlayer(gamePlayerTwoName, 'O')];
+    const startGame = (gamePlayerOneName, gamePlayerTwoName) => {
+        gamePlayers = [new gamePlayer(gamePlayerOneName, 'X'), new gamePlayer(gamePlayerTwoName, 'O')];
         currentPlayerIndex = 0;
         gameOver = false;
         gameBoard.resetBoard();
         displayController.drawBoard();
         displayController.updateGameStatus(`It's ${gamePlayers[currentPlayerIndex].name}'s turn to mark '${gamePlayers[currentPlayerIndex].mark}'.`);
-    }
+    };
 
-    const inGame () {
+    const inGame = (index) => {
+        if (!gameOver && gameBoard.setField(index, gamePlayers[currentPlayerIndex].mark)) {
+            displayController.drawBoard();
+            if (finishGame()) {
+                gameOver = true;
+                displayController.updateGameStatus(`${gamePlayers[currentPlayerIndex].name} wins!`);
+            } else if (gameBoard.getBoard().every(field => field !== '')) {
+                gameOver = true;
+                displayController.updateGameStatus("It's a tie!");
+            } else {
+                currentPlayerIndex = (currentPlayerIndex + 1) % 2;
+                displayController.updateGameStatus(`${gamePlayers[currentPlayerIndex].name}'s turn`);
+            }
+        }
+    };
 
-    }
-    const checkWinner = function () {
+    const finishGame = () => {
         const winningCombinations = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
@@ -44,67 +123,15 @@ const gameController = (function() {
         }
         return false;
     };
-    return {starGame, inGame, finishGame}
+
+    return { startGame, inGame, finishGame };
 })();
 
-// 3 gameBoard: It needs to be represented as an array and should be able to reset.
-// Manage the state of the gameBoard.
-const gameBoard = (function() {
-    
-    //declare the board to ''
-    let board = Array(9).fill('');
-    
-    //Updates the board with x or o (index) if the position is empty.
-    const setField (index, mark) {
-        if (index >= 0 && index < board.length && board[index] === '') {
-            board[index] = mark;
-            return true;
-        }
-        return false;
-    };
+// Initialize game setup
+function initializeGame() {
+    gameController.startGame()
+    displayController.drawBoard();
+}
 
-    //getField(index): Returns the value at a specific index.
-    const getField (index) {
-        return board[index];
-    };
-
-    //resetBoard(): Clears the board for a new game.
-    const resetBoard () {
-        board = Array(9).fill('');
-    };
-
-    //getBoard(): Returns the current state of the board.
-    const getBoard () {
-        return board;
-    };
-
-    return { setField, getField, resetBoard, getBoard }
-})();
-
-//4 displayController Manage game flow, check for a winner, handle player turns, and update the game status.
-
-const displayController = (function() {
-    //doesn't need to return anything, just wrapping bunch of functions
-    const gameStatusElement =document.getElementById('gameStatus')
-
-
-
-    startButton.addEventListener
-
-    restartButton.addEventListener
-
-    gameBoardElement.addEventListener
-
-    drawBoard
-     ga
-
-    const updateGameStatus (message) {
-        gameStatusElement.textContent = message;
-    }
-
-    return {drawBoard, updateGameStatus}
-
-})();
-
-//initial game
-displayController.drawBoard();
+// Call the setup function to initialize the game
+initializeGame();
